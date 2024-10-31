e04c81c9d53a3ec98480c644a26f516c7d1eab86
Ian <ian.mosonik@student.moringaschool.com>
2024-10-31 17:48:10 +0300

    completed lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function change(event){
+        event.preventDefault()
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input onChange={change} type="password"/>
+            {/* <h1>hi</h1> */}
+        </div>
     )
 }
 
